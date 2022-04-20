94c1ab458a8af859212b33fab1beb5f66e6a6054
kiikoh <ikikomonkkey@gmail.com>
2022-04-20 14:37:51 -0400

    update pdf merge command

--- mergePDFs.sh
+++ mergePDFs.sh
@@ -1,1 +1,1 @@
-pdfunite javascript/javascript.pdf cobol/Cobol.pdf basic/Basic.pdf pascal/Pascal.pdf scala/Scala.pdf FunctionalProgramming.pdf
+pdfunite lisp/lisp.pdf ml/ml.pdf erlang/erlang.pdf javascript/javascript.pdf scala/Scala.pdf FunctionalProgramming.pdf
