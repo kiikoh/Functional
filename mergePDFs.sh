73f47757482d4f09eaac5afa033ce30ba20e52df
kiikoh <ikikomonkey@gmail.com>
2022-04-21 12:06:09 -0400

    generate final doc

--- mergePDFs.sh
+++ mergePDFs.sh
@@ -1,1 +1,1 @@
-pdfunite lisp/lisp.pdf ml/ml.pdf erlang/erlang.pdf javascript/javascript.pdf scala/Scala.pdf FunctionalProgramming.pdf
+pdfunite lisp/lisp.pdf ml/ml.pdf erlang/erlang.pdf javascript/javascript.pdf scala/scala.pdf FunctionalProgramming.pdf
